5fc467257ee76bb07ad9533fa7ea54d6aafe5b0f
GrishmaSuresh <145242689+GrishmaSuresh@users.noreply.github.com>
2024-12-11 21:25:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,4 +63,4 @@ def export_data(table):
             connection.close()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
